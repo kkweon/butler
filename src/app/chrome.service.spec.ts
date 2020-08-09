c05de1ee27f5257aa2c22b187434f20fd19104c7
Mo Kweon <kkweon@google.com>
2020-08-08 21:14:10 -0700

    Ran prettier

--- src/app/chrome.service.spec.ts
+++ src/app/chrome.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { ChromeService } from './chrome.service';
+import { ChromeService } from './chrome.service'
 
 describe('ChromeService', () => {
-  let service: ChromeService;
+  let service: ChromeService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ChromeService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(ChromeService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
