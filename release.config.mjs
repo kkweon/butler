a57039ec43164f3b652e1e35f470e3fe003a10f9
Mo Kweon <kkweon@google.com>
2025-05-31 23:38:45 -0700

    chore(release): Migrate semantic-release config to ES module format
    This commit updates the configuration file for semantic-release from a JSON file (.releaserc) to an ES Module JavaScript file (release.config.mjs).
    
    The configuration content and its functionality remain identical. This change aligns with modern JavaScript practices and enables potential for more dynamic configuration in the future.

---
+++ release.config.mjs
@@ -0,0 +1,25 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      'semantic-release-chrome',
+      {
+        asset: 'butler.zip',
+        distFolder: 'dist/butler',
+        extensionId: 'haepoecmeobjjfeonmpphmpajaefcnfo',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: ['butler.zip'],
+      },
+    ],
+  ],
+  publish: ['@semantic-release/github'],
+}
