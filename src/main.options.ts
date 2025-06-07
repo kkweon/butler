d10c9c08214cc7fd10e5eeaf3215155cfb85f88b
Mo Kweon <kkweon@google.com>
2025-06-07 13:34:36 -0700

    feat: Implement multi-entrypoint build for popup and options

--- src/main.options.ts
+++ src/main.options.ts
@@ -1,15 +1,15 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { enableProdMode, importProvidersFrom } from '@angular/core'
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
-import { OptionsComponent } from './app/options.component';
-import { environment } from './environments/environment';
+import { OptionsComponent } from './app/options.component'
+import { environment } from './environments/environment'
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
 bootstrapApplication(OptionsComponent, {
@@ -19,8 +19,8 @@ bootstrapApplication(OptionsComponent, {
       BrowserAnimationsModule,
       MatFormFieldModule,
       MatInputModule,
-      MatSlideToggleModule
+      MatSlideToggleModule,
     ),
     // No router needed for options page currently
   ],
-}).catch((err) => console.error(err));
+}).catch((err) => console.error(err))
